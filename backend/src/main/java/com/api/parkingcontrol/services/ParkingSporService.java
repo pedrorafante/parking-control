f6f61b55ee9f534d2ee25df3b41aaa5ee8fda9a6
Pedro Rafante <pedrorafante@gmail.com>
2022-02-20 13:06:40 -0300

    Adicionado Paginação

--- backend/src/main/java/com/api/parkingcontrol/services/ParkingSporService.java
+++ backend/src/main/java/com/api/parkingcontrol/services/ParkingSporService.java
@@ -1,9 +1,10 @@
 package com.api.parkingcontrol.services;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,8 +37,8 @@ public class ParkingSporService {
 		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
 	}
 
-	public List<ParkingSpotModel> findAll() {
-		return parkingSpotRepository.findAll();
+	public Page<ParkingSpotModel> findAll(Pageable pageable) {
+		return parkingSpotRepository.findAll(pageable);
 	}
 
 	public Optional<ParkingSpotModel> findById(UUID id) {
