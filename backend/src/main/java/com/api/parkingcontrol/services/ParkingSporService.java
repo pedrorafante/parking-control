fd81ba49b075e9488f5d7722c9a5061b6ab80a8e
Pedro Rafante <pedrorafante@gmail.com>
2022-02-20 12:41:32 -0300

    Implementado método delete

--- backend/src/main/java/com/api/parkingcontrol/services/ParkingSporService.java
+++ backend/src/main/java/com/api/parkingcontrol/services/ParkingSporService.java
@@ -44,4 +44,9 @@ public class ParkingSporService {
 		return parkingSpotRepository.findById(id);
 	}
 
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		parkingSpotRepository.delete(parkingSpotModel);
+	}
+
 }
